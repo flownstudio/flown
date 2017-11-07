3009921ea718fa9bee7573e0b55a42951138b98b
Isaac Clarke <isaac@ishimura.uk>
2017-11-07 15:56:34 +0000

    metas

--- Assets/SandboxIsaac/player.cs
+++ Assets/SandboxIsaac/player.cs
@@ -28,21 +28,21 @@ public class player : MonoBehaviour {
 		distanceFromGround = Vector3.Distance(this.transform.position, groundPoint);
 	}
 
-	// UNCOMMENT TO MAKE SOUND
-	// void OnAudioFilterRead(float[] data, int channels)
-	// {
-	// 	// update increment in case frequency has changed
-	// 	increment = frequency * distanceFromFlock * Math.PI / sampling_frequency;
+	//UNCOMMENT TO MAKE SOUND
+	void OnAudioFilterRead(float[] data, int channels)
+	{
+		// update increment in case frequency has changed
+		increment = frequency * distanceFromFlock * Math.PI / sampling_frequency;
 
-	// 	for (var i = 0; i < data.Length; i = i + channels)
-	// 	{
-	// 		phase = phase + increment;
-	// 		// this is where we copy audio data to make them “available” to Unity
-	// 		data[i] = (float)(gain*Math.Sin(phase));
-	// 		// if we have stereo, we copy the mono data to each channel
-	// 		if (channels == 2) data[i + 1] = data[i];
-	// 		if (phase > 2 * Math.PI) phase = 0;
-	// 	}
-	// }
+		for (var i = 0; i < data.Length; i = i + channels)
+		{
+			phase = phase + increment;
+			// this is where we copy audio data to make them “available” to Unity
+			data[i] = (float)(gain*Math.Sin(phase));
+			// if we have stereo, we copy the mono data to each channel
+			if (channels == 2) data[i + 1] = data[i];
+			if (phase > 2 * Math.PI) phase = 0;
+		}
+	}
 
 }
