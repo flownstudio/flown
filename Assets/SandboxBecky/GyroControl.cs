b228226b385f2148a6ad844449b3a7df200ac91f
Rebecca Aston <rebecca.l.aston@gmail.com>
2017-11-07 15:11:42 +0000

    Desktop Mouse Look Controller

--- Assets/SandboxBecky/GyroControl.cs
+++ Assets/SandboxBecky/GyroControl.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GyroControl : MonoBehaviour {
+	//Publib variables
+	public GameObject target;
+	public float speed;
 
 	private bool gyroEnabled;
 	private Gyroscope gyro;
@@ -10,10 +13,16 @@ public class GyroControl : MonoBehaviour {
 	private GameObject cameraContainer;
 	private Quaternion rot;
 
+	//GUI string do mobile Debug
+	private string eulerVals;
+
 	// Use this for initialization
 	void Start () {
 		cameraContainer = new GameObject ("Camera Container");
+
+		//transforms cameraContainer to camera position
 		cameraContainer.transform.position = transform.position;
+		//Sets cameraConainer as parent of camera
 		transform.SetParent (cameraContainer.transform);
 
 		gyroEnabled = EnableGyro();
@@ -25,8 +34,8 @@ public class GyroControl : MonoBehaviour {
 			gyro = Input.gyro;
 			gyro.enabled = true;
 
-			cameraContainer.transform.rotation = Quaternion.Euler (90f, 90f, 0f);
-			rot = new Quaternion (0, 0, 1, 0);
+//			cameraContainer.transform.rotation = Quaternion.Euler (90f, 90f, 0f);
+//			rot = new Quaternion (0, 0, 1, 0);
 
 			return true;
 		}
@@ -35,7 +44,41 @@ public class GyroControl : MonoBehaviour {
 		
 	private void Update(){
 		if (gyroEnabled) {
-			transform.localRotation = gyro.attitude * rot;
+			float compareAngle; 
+			float step = speed * Time.deltaTime;
+
+			//Compare the Quaternions... i.e. if Gyro is in LookRotation range Feeback == GOOD
+			Vector3 relativePos = target.transform.position - transform.position;
+			Quaternion rotation = Quaternion.LookRotation(relativePos);
+			//		//Don't transform
+//					transform.localRotation = rotation;
+
+			transform.localRotation = gyro.attitude ;
+
+			Debug.Log ("localRotation: " + transform.localRotation);
+			Debug.Log ("Rotation: " + transform.rotation);
+			Debug.Log ("LookRotation: " + rotation);
+
+			compareAngle = Quaternion.Angle(transform.localRotation, rotation);
+
+			if (compareAngle < 100) {
+				speed = 3;
+			} else {
+				speed = 1;
+			}
+
+			//Move The Camera towards the target
+			transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+
+
+//			eulerVals = "X: " + transform.localEulerAngles.x + "\n Y: " + transform.localEulerAngles.y + "\n Z: " + transform.localEulerAngles.z;
+			eulerVals = "ANGLE: " + compareAngle;
+
 		}
+			
+	}
+		
+	void OnGUI() {
+		GUI.Label(new Rect(10, 10, 500, 20), eulerVals);
 	}
 }
---
+++ Assets/SandboxBecky/dummyMoveTarget.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dummyMoveTarget : MonoBehaviour {
+	private Vector3 startMarker;
+	public float speed = 3.0F;
+	private float startTime;
+	private float journeyLength;
+	public GameObject marker;
+
+
+	void Start() {
+		startTime = Time.time;
+		startMarker = transform.position;
+
+		pickRandomPoint();
+
+	}
+
+	void Update() {
+		float distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / journeyLength;
+
+//		Debug.Log (fracJourney);
+		if (fracJourney >= 1.0) {
+			startTime = Time.time;
+			startMarker = marker.transform.position;
+			pickRandomPoint ();
+
+		}
+		transform.position = Vector3.Lerp(startMarker, marker.transform.position, fracJourney);
+
+	}
+
+	void pickRandomPoint() {
+		Vector3 newPoint = new Vector3 (Random.Range (-30.0f, 30.0f), Random.Range (-30.0f, 30.0f), Random.Range (-30.0f, 30.0f));
+		marker.transform.position = newPoint;
+		journeyLength = Vector3.Distance(startMarker, marker.transform.position);
+	}
+}
