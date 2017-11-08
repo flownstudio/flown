b5f266e42ef1ee9e793e46dc78fc6bc433747d40
Isaac Clarke <ikeyjoe3@googlemail.com>
2017-11-08 11:49:50 +0000

    playing with audio

--- Assets/SandboxIsaac/player.cs
+++ Assets/SandboxIsaac/player.cs
@@ -7,18 +7,28 @@ public class player : MonoBehaviour {
 
 	public float distanceFromFlock;
 	public float distanceFromGround;
+	public float volumeLevel = 0.0f;
+	// public double frequency = 440;
+	// public double gain = 0.05;
 
-	public double frequency = 440;
-	public double gain = 0.05;
-
-	private double increment;
-	private double phase;
-	private double sampling_frequency = 48000;
-
+	// private double increment;
+	// private double phase;
+	// private double sampling_frequency = 48000;
+	AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
 		
+
+		audioSource = gameObject.AddComponent<AudioSource>();
+
+ 		audioSource.clip = Resources.Load("isaacsTestResources/Track2Loop - Agitated strings") as AudioClip;
+
+ 		audioSource.loop = true;
+ 		audioSource.volume = volumeLevel;
+ 		audioSource.spatialBlend = 1.0f;
+ 		audioSource.Play();
+
 	}
 
 	// Update is called once per frame
@@ -26,23 +36,32 @@ public class player : MonoBehaviour {
 		distanceFromFlock = Vector3.Distance(globalFlock.headingPos, this.transform.position);
 		Vector3 groundPoint = new Vector3(this.transform.position.x, 0, this.transform.position.z);
 		distanceFromGround = Vector3.Distance(this.transform.position, groundPoint);
+
+		if(distanceFromFlock < 6){
+			volumeLevel = 0.0f;
+		}
+		else{
+			volumeLevel = distanceFromFlock * 0.01f;
+		}
+		
+		audioSource.volume = volumeLevel;
 	}
 
 	//UNCOMMENT TO MAKE SOUND
-	void OnAudioFilterRead(float[] data, int channels)
-	{
-		// update increment in case frequency has changed
-		increment = frequency * distanceFromFlock * Math.PI / sampling_frequency;
+	// void OnAudioFilterRead(float[] data, int channels)
+	// {
+	// 	// update increment in case frequency has changed
+	// 	increment = frequency * distanceFromFlock * Math.PI / sampling_frequency;
 
-		for (var i = 0; i < data.Length; i = i + channels)
-		{
-			phase = phase + increment;
-			// this is where we copy audio data to make them “available” to Unity
-			data[i] = (float)(gain*Math.Sin(phase));
-			// if we have stereo, we copy the mono data to each channel
-			if (channels == 2) data[i + 1] = data[i];
-			if (phase > 2 * Math.PI) phase = 0;
-		}
-	}
+	// 	for (var i = 0; i < data.Length; i = i + channels)
+	// 	{
+	// 		phase = phase + increment;
+	// 		// this is where we copy audio data to make them “available” to Unity
+	// 		data[i] = (float)(gain*Math.Sin(phase));
+	// 		// if we have stereo, we copy the mono data to each channel
+	// 		if (channels == 2) data[i + 1] = data[i];
+	// 		if (phase > 2 * Math.PI) phase = 0;
+	// 	}
+	//}
 
 }
