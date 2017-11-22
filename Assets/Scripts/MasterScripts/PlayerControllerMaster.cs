26477211ddab66db14a986ac04e36908e2aa23ba
joseph <contact@helloidiot.co.uk>
2017-11-22 15:49:08 +0000

    Everything combined into main folder structure

--- Assets/Scripts/MasterScripts/PlayerControllerMaster.cs
+++ Assets/Scripts/MasterScripts/PlayerControllerMaster.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerControllerMaster : MonoBehaviour {
 
 	//Public variables
 	public Swipe swipeControls;
