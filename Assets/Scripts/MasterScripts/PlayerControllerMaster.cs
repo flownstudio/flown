70d59068092fe45a0976668e3d3266d6269f7c46
Rebecca Aston <rebecca.l.aston@gmail.com>
2017-12-05 14:36:33 +0000

    added an arrow to the wind tunnel sprites

--- Assets/Scripts/MasterScripts/PlayerControllerMaster.cs
+++ Assets/Scripts/MasterScripts/PlayerControllerMaster.cs
@@ -9,17 +9,22 @@ public class PlayerControllerMaster : MonoBehaviour {
 	public GameObject sceneController;
 
 	public Camera attachedCamera;// Model that we are putting in front of camera
-//	public GameObject birdModel;
-//	public Vector3 spaceBuffer;
+	//	public GameObject birdModel;
+	//	public Vector3 spaceBuffer;
 
 	private Rigidbody rigidbody;
-	public float speed = 30.0F;
+	public float speed = 0.0f;
+	private float minSpeed = 5.0f;
+	private float maxSpeed = 95.0f;
+	private float acceleration = 10.0f;
+	private float deceleration = 10.0f;
+	private float flapDistance;
+	private bool isFlapping = false;
 	private float distance, eulerZ, eulerX, eulerY;
 
 	private bool gyroEnabled;
 	private Gyroscope gyro;
 	private sceneController playerStats;
-
 
 	//-------------------------------
 	//Gyro calibration variables
@@ -40,6 +45,7 @@ public class PlayerControllerMaster : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
 		gyroEnabled = EnableGyro();
 		playerStats = sceneController.GetComponent<sceneController> ();
 		rigidbody = transform.GetComponent<Rigidbody>();
@@ -67,26 +73,18 @@ public class PlayerControllerMaster : MonoBehaviour {
 		// Now you have the player stats available
 		// playerStats.autoPilot
 		// etc (check sceneController)
-<<<<<<< HEAD
 
 
 		if (gyroEnabled && !playerStats.autoPilot) {
 			GyroPlayer ();
-=======
->>>>>>> parent of a031ee9... Updated player controller
 
+			float bias = 0.96f;
 
-<<<<<<< HEAD
 			Vector3 moveCamTo = transform.position - transform.forward * 7.0f + transform.up * 5.0f; // camera offset positions
 			attachedCamera.transform.position = attachedCamera.transform.position * bias + moveCamTo * (1.0f - bias); // moves the camera, elastically and bias
 			attachedCamera.transform.LookAt (transform.position + transform.forward * 30.0f); // rotating the camera
 
 		} else if (!playerStats.autoPilot) {
-=======
-		if(gyroEnabled){
-			GyroPlayer ();
-		}else{
->>>>>>> parent of a031ee9... Updated player controller
 
 			float bias = 0.96f;
 
@@ -97,26 +95,30 @@ public class PlayerControllerMaster : MonoBehaviour {
 
 			//attachedCamera.transform.lookRotation (0.0f, 0.0f, Input.GetAxis ("Horizontal"));
 
+			//increase slowdown rate
 			speed -= transform.forward.y;
 
-			if (speed < 5.0f) {
-				speed = 5.0f;
+			//Cap speed to minimum value
+			if (speed <= minSpeed) {
+				speed = minSpeed;
 			}
 
-			transform.position += transform.forward * Time.deltaTime * speed;
+			//Cap to maxSpeed
+			if (speed >= maxSpeed) {
+				speed = maxSpeed;
+			}
+
+			//COMMON FILTER - GRPAHING GYRO DATA
+
+			transform.position += transform.forward * speed * Time.deltaTime;
 			transform.Rotate (Input.GetAxis("Vertical"), 0.0f, Input.GetAxis ("Horizontal"));//HOW TO TRANSLATE DIRECTION TO MOBILE?? TIN!!!!
 			//birdModel.transform.position = transform.position + spaceBuffer;
 
 
 
 			//Debug
-<<<<<<< HEAD
-//			Debug.Log("DISTANCE: " + distance);
-//			Debug.Log("SPEED: " + speed);
-=======
-			Debug.Log("DISTANCE: " + distance);
-			Debug.Log("SPEED: " + speed);
->>>>>>> parent of a031ee9... Updated player controller
+			//			Debug.Log("DISTANCE: " + distance);
+			//			Debug.Log("SPEED: " + speed);
 
 			//float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight( transform.position );
 			//if (terrainHeightWhereWeAre > transform.position.y){
@@ -136,31 +138,63 @@ public class PlayerControllerMaster : MonoBehaviour {
 	//This is for physics updates
 	private void FixedUpdate(){
 
+		//DESKTOP MOVEMENT
+
+		if (swipeControls.Tap) {
+			flap();
+		}
+
+		//MOBILE MOVEMENT
 
 		if(gyroEnabled){
 			TransformPlayer ();
 
-			if (swipeControls.SwipeLeft) {
-				//			if(speed < 0)speed *= -1;
-				//			rigidbody.AddRelativeForce (transform.forward * speed,ForceMode.Acceleration);
-				speed += 2.0F;
+			if (swipeControls.Tap) {
+				flap();
 			}
 
-			if (swipeControls.SwipeRight && speed > 1.0F) {
-				//			if(speed > 0)speed *= -1;
-				//			rigidbody.AddRelativeForce (transform.forward * speed,ForceMode.Acceleration);
-
-				speed -= 1.0F;
-			}
 
 		}else{
-			//Move megans stuff in here too 
+			//Move megans stuff in here too
 		}
-			
+
 
 
 
 	}
+
+
+
+	private void flap()
+	{
+		//increase speed
+		isFlapping = true;
+
+		if (isFlapping)
+		{
+			speed += acceleration;
+			StartCoroutine(boostOnYAxis());
+			//Lerp transform position upwards
+			//transform.position += transform.up * Time.deltaTime * 500f;
+
+			//transform.localPosition = Vector3.Lerp (transform.position, new Vector3 (transform.position.x, transform.position.y + jump, transform.position.z), 10);
+			//Debug.Log (transform.position);
+		}
+		else
+		{
+			isFlapping = false;
+		}
+	}
+
+	IEnumerator boostOnYAxis(){
+		flapDistance = 50.0f;
+		rigidbody.velocity = new Vector3 (0, flapDistance, 0);
+		yield return new WaitForSeconds(0.01f);
+		rigidbody.velocity = new Vector3 (0, -flapDistance / 2, 0);
+		yield return new WaitForSeconds(0.2f);
+		rigidbody.velocity = new Vector3(0, 0, 0);
+	}
+
 
 	//For Physics
 	private void TransformPlayer(){
@@ -173,18 +207,33 @@ public class PlayerControllerMaster : MonoBehaviour {
 
 		rigidbody.MovePosition(transform.position + transform.forward * Time.deltaTime * speed);
 
+		//increase slowdown rate
+		speed -= transform.forward.y;
+
+		//Cap speed to minimum value
+		if (speed <= minSpeed) {
+			speed = minSpeed;
+		}
+
+		//Cap to maxSpeed
+		if (speed >= maxSpeed) {
+			speed = maxSpeed;
+		}
+
+
+
 		//		for when looking down AND then subtract when looking up
 		//		rigidbody.AddRelativeForce (transform.forward * speed,ForceMode.Acceleration);
 		//		rigidbody.velocity = transform.forward * Time.deltaTime * speed;
 
-//		Debug.Log(transform.forward + " " + Vector3.forward);
+		//		Debug.Log(transform.forward + " " + Vector3.forward);
 
-		//		localVelocity = transform.InverseTransformDirection(rigidbody.velocity);  
+		//		localVelocity = transform.InverseTransformDirection(rigidbody.velocity);
 	}
 
 	//Gyro and/or look around
 	private void GyroPlayer(){
-		
+
 		Quaternion test = Quaternion.Slerp(transform.rotation,
 			cameraBase * ( ConvertRotation(referanceRotation * Input.gyro.attitude) ), lowPassFilterFactor);
 		transform.rotation = test;
@@ -195,11 +244,10 @@ public class PlayerControllerMaster : MonoBehaviour {
 
 
 
-
 		//`maybe to figure out direction
 
 		//		if (eulerY > 180) {
-		////			attachedCamera.transform 
+		////			attachedCamera.transform
 		//		} else if (eulerY < 180) {
 		//
 		//		}
@@ -210,23 +258,14 @@ public class PlayerControllerMaster : MonoBehaviour {
 		//			speed -= 1.0F;
 		//		}
 
-	
+
 	}
-
-
-	//TODO: make autopilot.... 
-	// Now you have the player stats available
-	// playerStats.autoPilot
-
 
 	void OnGUI() {
 		//		string gui = "X: " + eulerX + ", Y: " + eulerY +", Z: " + eulerZ;
 		string gui = "Speed: " + speed;
 		GUI.Label(new Rect(10, 10, 500, 20), gui );
 	}
-
-
-
 
 
 
@@ -295,7 +334,7 @@ public class PlayerControllerMaster : MonoBehaviour {
 	/// </param>
 	private static Quaternion ConvertRotation(Quaternion q)
 	{
-		return new Quaternion(q.x, q.y, -q.z, -q.w);	
+		return new Quaternion(q.x, q.y, -q.z, -q.w);
 	}
 
 
@@ -314,5 +353,4 @@ public class PlayerControllerMaster : MonoBehaviour {
 	{
 		referanceRotation = Quaternion.Inverse(baseOrientation)*Quaternion.Inverse(calibration);
 	}
-
 }
